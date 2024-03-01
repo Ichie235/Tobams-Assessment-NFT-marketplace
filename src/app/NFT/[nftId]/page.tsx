9616bb7f6bf6b4b830c02e6fa50d3fbf11d18c6c
Ichie Chinemerem Richard <108372995+Ichie235@users.noreply.github.com>
2024-03-01 03:40:53 +0100

    add mobile responsiveness

--- src/app/NFT/[nftId]/page.tsx
+++ src/app/NFT/[nftId]/page.tsx
@@ -8,6 +8,9 @@ import {
 import ToggleButton from "@/app/Components/Button/ToggleButton";
 import PopularNftCard from "@/app/Components/NftCard/PopularNftCard";
 import Footer from "@/app/Components/Footer/Footer";
+import CopyIcon from "../../../../svg/CopyIcon";
+import ShareIcon from "../../../../svg/ShareIcon";
+import HeartIcon from "../../../../svg/HeartIcon";
 
 type Params = {
   params: {
@@ -26,7 +29,7 @@ export default function NftProjects({ params: { nftId } }: Params) {
 
   return (
     <>
-      <main className="flex flex-col items-start justify-center px-32 py-24">
+      <main className="relative flex flex-col items-start justify-center px-6 py-32 md:px-11   lg:px-20 ">
         <section className="grid grid-cols-1 lg:grid-cols-2 gap-16 xl:gap-24 border border-nft-light-gray rounded-xl p-3">
           <div>
             <Image
@@ -34,24 +37,33 @@ export default function NftProjects({ params: { nftId } }: Params) {
               width={684}
               height={885}
               alt={projectDetails.name}
-              className="rounded-xl w-[250px] h-[300px] md:w-[450px] md:h-[600px] lg:w-[684px] lg:h-[100vh] md:mx-auto lg:mx-0"
+              className="rounded-xl w-[250px] h-[300px] md:w-[450px] md:h-[600px] lg:w-[684px] lg:h-[100vh] mt-2 md:mt-0 mx-auto lg:mx-0"
             />
           </div>
-          <div className="mx-5 lg:mx-0">
-            <h1 className="w-full xl:w-[469px] text-white font-semibold text-2xl md:text-3xl xl:text-4xl mt-8">
+          <span className="absolute  -right-1 md:right-4 lg:right-12 top-48">
+            <HeartIcon />
+          </span>
+          <span className=" absolute -right-1 md:right-4 lg:right-12 top-[17rem]">
+            <CopyIcon />
+          </span>
+          <span className="absolute -right-1 md:right-4 lg:right-12 top-[23rem]">
+            <ShareIcon />
+          </span>
+          <div className="mx-2 md:mx-5 lg:mx-0">
+            <h1 className="w-full xl:w-[469px] text-white font-semibold text-2xl md:text-3xl xl:text-4xl mt-3 md:mt-8">
               {projectDetails.name}
             </h1>
-            <p className="text-nft-deep-gray my-4">
+            <p className="text-nft-deep-gray my-4 lg:px-1 xl:px-0">
               {projectDetails.description}
             </p>
-            <div className="flex gap-9 xl:gap-12 my-5 xl:my-9">
-              <div className="flex gap-1">
+            <div className="flex flex-col  md:flex-row gap-5 md:gap-9 xl:gap-12 my-5 xl:my-9">
+              <div className="flex -ml-7">
                 <Image
                   src={projectDetails.creatorImage}
                   width={90}
                   height={90}
                   alt={projectDetails.name}
-                  className="rounded-full lg:w-[70px] lg:h-[70px] xl:w-[90px] xl:h-[90px]"
+                  className="rounded-full  lg:w-[70px] lg:h-[70px] xl:w-[90px] xl:h-[90px]"
                 />
                 <div className="flex flex-col justify-center  gap-2 text-white">
                   <p className="text-nft-deep-gray">{projectDetails.creator}</p>
@@ -72,8 +84,8 @@ export default function NftProjects({ params: { nftId } }: Params) {
                 </div>
               </div>
             </div>
-            <div className="bg-nft-white-on-black border border-[#262840] mr-10 px-6 py-4 rounded-2xl">
-              <div className="grid grid-cols-2 gap-12 px-6">
+            <div className="bg-nft-white-on-black border border-[#262840] md:mr-10 px-2 py-8 md:px-6 md:py-4 rounded-2xl">
+              <div className="grid grid-cols-2 gap-12 px-3 md:px-6">
                 <div>
                   <p className="text-nft-deep-gray">Current Price</p>
                   <h1 className="text-3xl text-white font-bold">
@@ -107,12 +119,12 @@ export default function NftProjects({ params: { nftId } }: Params) {
             </div>
           </div>
         </section>
-        <section className="flex flex-col items-center gap-7 mx-2 md:mx-10 lg:mx-16 my-36 ">
+        <section className="flex flex-col items-center gap-7  my-36 ">
           <p className="text-nft-deep-gray text-base text-center">OVERLINE</p>
           <h1 className="text-white text-4xl text-center mt-1">
             Most popular live auctions
           </h1>
-          <div className="flex flex-col md:grid md:grid-cols-3 lg:flex lg:flex-row lg:justify-center lg:items-center  gap-6">
+          <div className="flex flex-col md:grid md:grid-cols-3 xl:flex xl:flex-row xl:justify-center xl:items-center  gap-4">
             {PopularNFTproject.map((item, index) => (
               <div key={index}>
                 <PopularNftCard
